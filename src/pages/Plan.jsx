2d11c1ee573a12c5d88b9adfe3b2822d54acd747
steveoh <sgourley@utah.gov>
2021-09-27 12:05:17 -0600

    style: add back and api key to plan

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useGeocodeContext } from '../components/GeocodeContext';
 import humanizeDuration from 'humanize-duration';
+import { Link } from 'react-router-dom';
+import { useGeocodeContext } from '../components/GeocodeContext';
+import ApiKey from '../components/ApiKey.jsx';
 
 export default function Plan() {
   const geocodingContext = useGeocodeContext()[0];
@@ -11,13 +13,21 @@ export default function Plan() {
     window.ugrc.getRecordCount(geocodingContext.file.path).then(setRows);
   }, [geocodingContext.file.path]);
 
+  const start = () => {};
+
   return (
-    <div>
+    <>
+      <ApiKey />
+      <Link className="px-4 py-1 text-white bg-indigo-400 border border-indigo-600 rounded shadow" to="/data">
+        &larr; Back
+      </Link>
       <p>
         You have selected {geocodingContext.file.name} with {rows} records.
       </p>
       <p>We estimate this will take approximately {humanizeDuration(duration, { round: true })}.</p>
-      <button>Start</button>
-    </div>
+      <button onClick={start} type="button">
+        Start
+      </button>
+    </>
   );
 }
