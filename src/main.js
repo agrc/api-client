977236706d066a2faaf338fe5a644d3c4d78dfd6
stdavis <stdavis@utah.gov>
2021-10-04 14:37:16 -0600

    fix: open dev tools in dev only

--- src/main.js
+++ src/main.js
@@ -26,7 +26,9 @@ const createWindow = () => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.webContents.openDevTools();
+  }
 };
 
 // This method will be called when Electron has finished
