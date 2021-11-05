ba1e5d5d44350e9b17c7444ee6cd2cb6ee52ebe6
stdavis <stdavis@utah.gov>
2021-11-05 16:05:06 -0600

    feat: add root error boundary
    Closes #72

---
+++ src/pages/Error.jsx
@@ -0,0 +1,41 @@
+export default function Error({ error }) {
+  return (
+    <article className="">
+      <div className="p-10">
+        <h2 className="my-0 text-indigo-600">This is a little embarrassing...</h2>
+        <p className="mt-10">
+          There was an error in the application that caused it to crash. Here are some technical details.{' '}
+          <span role="img" aria-label="nerd emoji">
+            🤓
+          </span>
+        </p>
+        {error && error.message ? (
+          <>
+            <label htmlFor="message">Error message:</label>
+            <pre id="message" className="overflow-auto text-base text-gray-400">{`${error.message}`}</pre>
+          </>
+        ) : null}
+        {error && error.stack ? (
+          <>
+            <label htmlFor="stack">Stack trace:</label>
+            <pre id="stack" className="overflow-auto text-base text-gray-400">{`${error.stack}`}</pre>
+          </>
+        ) : null}
+        {error && !error.message && !error.stack ? (
+          <pre className="overflow-auto text-base text-gray-400">{error.toString()}</pre>
+        ) : null}
+        <div className="flex justify-between">
+          <button type="back-button" onClick={window.ugrc.relaunchApp}>
+            Restart application
+          </button>
+          <button
+            type="back-button"
+            onClick={() => window.ugrc.openIssue({ message: error.message, stack: error.stack })}
+          >
+            Report this issue
+          </button>
+        </div>
+      </div>
+    </article>
+  );
+}
