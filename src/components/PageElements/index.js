8e51f940ee8613982f3be64dd4d7d092a2e38dbb
steveoh <sgourley@utah.gov>
2021-11-16 12:26:16 -0700

    feat: add spinner component

--- src/components/PageElements/index.js
+++ src/components/PageElements/index.js
@@ -2,5 +2,6 @@ import QuickApiKey from '../QuickApiKey.jsx';
 import Footer from '../Footer.jsx';
 import Chrome from '../Chrome.jsx';
 import Header from '../Header.jsx';
+import Spinner from '../Spinner.jsx';
 
-export { Chrome, QuickApiKey, Header, Footer };
+export { Chrome, QuickApiKey, Header, Footer, Spinner };
