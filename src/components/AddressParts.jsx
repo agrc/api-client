60f0912067eea070a8788f59647e619639ba10b1
steveoh <sgourley@utah.gov>
2021-11-22 17:27:10 -0700

    refactor: create component

--- src/components/AddressParts.jsx
+++ src/components/AddressParts.jsx
@@ -3,27 +3,24 @@ export default function AddressParts() {
     <section className="mt-6">
       <p>The UGRC API requires 2 inputs to geocode. The first is a street address in the form of</p>
       <section className="flex items-stretch justify-around w-full mb-10 text-center border divide-x-2 divide-gray-100 rounded-lg shadow-lg">
-        <div className="flex-1 p-4">
-          <h2 className="mb-0 text-indigo-600">301</h2>
-          <span className="block text-base text-gray-400">house number</span>
-        </div>
-        <div className="flex-1 p-4">
-          <h2 className="mb-0 text-indigo-600">South</h2>
-          <span className="block text-base text-gray-400">prefix direction</span>
-        </div>
-        <div className="flex-1 p-4">
-          <h2 className="mb-0 text-indigo-600">Main</h2>
-          <span className="block text-base text-gray-400">street name</span>
-        </div>
-        <div className="flex-1 p-4">
-          <h2 className="mb-0 text-indigo-600">Street</h2>
-          <span className="block text-base text-gray-400">street type or suffix direction</span>
-        </div>
+        <Label value="301" label="house number" />
+        <Label value="South" label="prefix direction" />
+        <Label value="Main" label="street name" />
+        <Label value="Street" label="street type or suffix direction" />
       </section>
       <p>
         The second required input is a zone. A zone can be a zip code or a city name. If your data has both available,
         prefer the zip code. This data needs to be structured data in a CSV format with a header row.
       </p>
     </section>
+  );
+}
+
+function Label({ label, value }) {
+  return (
+    <div className="flex-1 p-4">
+      <h2 className="mb-0 text-indigo-600">{value}</h2>
+      <span className="block text-base text-gray-400">{label}</span>
+    </div>
   );
 }
