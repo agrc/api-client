5c2e6dcb13a3a489454fa5cb2e54f9c663514441
steveoh <sgourley@utah.gov>
2022-01-18 15:50:06 -0700

    chore: allow renderer to invoke analytic methods

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,13 +1,14 @@
+const { ipcMain } = require('electron');
 const machineIdSync = require('node-machine-id').machineIdSync;
 const ua = require('universal-analytics');
 const { is } = require('electron-util');
-
 const id = machineIdSync();
 const visitor = ua('UA-11849964-68', id);
 
 visitor.event({ ec: 'application-open', ea: id, el: process.platform }).send();
 visitor.event({ ec: 'client-version', ea: id, el: process.version }).send();
 
+//* main thread functions
 export const trackEvent = ({ category, action = id, label }) => {
   visitor
     .event({
@@ -27,3 +28,13 @@ export const trackException = (ex, fatal = false) => {
 
   visitor.exception(ex, fatal).send();
 };
+
+//* renderer events
+ipcMain.on('trackEvent', (_, content) => {
+  console.log(content);
+  trackEvent(content);
+});
+
+ipcMain.on('trackException', (_, content) => {
+  trackException(content);
+});
