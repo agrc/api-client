bc0ade30f89de262d670f32f2507b749f9e0eb91
steveoh <sgourley@utah.gov>
2022-01-18 14:15:34 -0700

    refactor: don't log development errors

--- src/services/analytics.js
+++ src/services/analytics.js
@@ -1,10 +1,12 @@
 const machineIdSync = require('node-machine-id').machineIdSync;
 const ua = require('universal-analytics');
+const { is } = require('electron-util');
 
 const id = machineIdSync();
 const visitor = ua('UA-11849964-68', id);
 
 visitor.event({ ec: 'application-open', ea: id, el: process.platform }).send();
+visitor.event({ ec: 'client-version', ea: id, el: process.version }).send();
 
 export const trackEvent = ({ category, action = id, label }) => {
   visitor
@@ -14,4 +16,14 @@ export const trackEvent = ({ category, action = id, label }) => {
       el: label,
     })
     .send();
+};
+
+export const trackException = (ex, fatal = false) => {
+  if (is.development) {
+    console.error(ex);
+
+    return;
+  }
+
+  visitor.exception(ex, fatal).send();
 };
