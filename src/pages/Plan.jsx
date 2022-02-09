5ff562675c570778da89a7ff54c3ea56b86803b6
steveoh <sgourley@utah.gov>
2022-02-08 17:04:46 -0700

    chore: format numbers pretty

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -2,6 +2,8 @@ import humanizeDuration from 'humanize-duration';
 import { Link, useHistory } from 'react-router-dom';
 import { useGeocodeContext } from '../components/GeocodeContext';
 
+const numberFormat = new Intl.NumberFormat('en-US');
+
 export default function Plan() {
   const { geocodeContext } = useGeocodeContext();
   const history = useHistory();
@@ -17,9 +19,9 @@ export default function Plan() {
         &larr; Back
       </Link>
       <h2>The plan</h2>
-      <section className="flex items-stretch justify-around w-full mb-10 text-center border divide-x-2 divide-gray-100 rounded-lg shadow-lg">
+      <section className="mb-10 flex w-full items-stretch justify-around divide-x-2 divide-gray-100 rounded-lg border text-center shadow-lg">
         <div className="flex-1 p-6">
-          <h2 className="my-0 text-indigo-600">{geocodeContext.data.totalRecords}</h2>
+          <h2 className="my-0 text-indigo-600">{numberFormat.format(geocodeContext.data.totalRecords)}</h2>
           <span className="block text-base text-gray-400">addresses</span>
         </div>
         <div className="flex-1 p-6">
